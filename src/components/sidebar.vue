aab267ed4283ce0b9401c9ce81018579dd923e01
Unknown <mikeyredtiger22@gmail.com>
2018-04-22 14:18:50 +0100

    Fixed sidebar container sizing bug. Added content.

--- src/components/sidebar.vue
+++ src/components/sidebar.vue
@@ -1,28 +1,25 @@
 <template>
   <div class="col-12 col-md-3 col-lg-2 p-0 sidebar">
-    <nav class="navbar navbar-expand flex-column align-items-start py-2">
-      <div class="collapse navbar-collapse">
-        <ul class="flex-column navbar-nav w-100 justify-content-between">
-          <li class="nav-item">
-            <a class="nav-link pl-0 title" href="#">Society Scheduler</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link pl-0" href="#">Link 1</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link pl-0 heading" href="#">Link 2</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link pl-0" href="#">Link 3</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link pl-0 item" href="#">Link 4</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link pl-0" href="#">Link 5</a>
-          </li>
-        </ul>
-      </div>
+    <nav class="navbar navbar-expand flex-column align-items-start p-0">
+      <ul class="flex-column navbar-nav w-100 justify-content-between">
+        <!--sidebar content-->
+        <li class="nav-item pb-4 title m-3">Society Scheduler
+        </li>
+        <!--Committees-->
+        <li v-if="userSocietiesInfo.committees" class="nav-item border-bottom border-top border-secondary">
+          <a class="m-3 heading">Committees</a>
+        </li>
+        <li v-for="society in userSocietiesInfo.committees" :key="society._id" class="ml-3 py-1 item">
+          <a v-on:click="committeeClick(society)" href="#" >{{society.name}}</a>
+        </li>
+        <!--Joined Societies-->
+        <li v-if="userSocietiesInfo.joined" class="nav-item border-bottom border-top border-secondary">
+          <a class="m-3 heading">My Societies</a>
+        </li>
+        <li v-for="society in userSocietiesInfo.joined" :key="society._id" class="ml-3 py-1 item">
+          <a v-on:click="committeeClick(society)" href="#" >{{society.name}}</a>
+        </li>
+      </ul>
     </nav>
   </div>
     <!--<div class="sidebar col-12 col-md-3 col-lg-4">
@@ -55,10 +52,13 @@
 <script>
 export default {
   name: 'sidebar',
-  computed: {
-    getUserSocietiesInfo() {
-      return this.$store.state.userSocietiesInfo;
-    }
+  data: function() {
+    return {
+      userSocietiesInfo: {}
+    };
+  },
+  created: function() {
+    this.userSocietiesInfo = this.$store.state.userSocietiesInfo;
   }
 };
 </script>
@@ -75,7 +75,7 @@ export default {
   font-size: 1.5rem;
 }
 .item {
-  font-size: 1rem;
+  font-size: 1.25rem;
   display: block;
 }
 </style>
